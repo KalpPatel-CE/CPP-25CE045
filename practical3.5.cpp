49d6cc333e2770acfc682e82424576fbb7d0c386
KalpPatel-CE <kalppatel.ce@gmail.com>
2025-04-14 14:59:50 +0530

    Create practical3.5.cpp

---
+++ practical3.5.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int superDigit(int num) {
+    if (num < 10) {
+        return num;
+    }
+
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    return superDigit(sum);
+}
+
+int main() {
+    string n;
+    int k;
+    
+    cin >> n;
+    cin >> k;
+
+    int initialSum = 0;
+    for (char digit : n) {
+        initialSum += digit - '0';
+    }
+
+    int totalSum = initialSum * k;
+
+    int result = superDigit(totalSum);
+    cout << result << endl;
+
+    return 0;
+}
