9a58562377f9160e97e3a09dd4fc9bdf6249bc1e
KalpPatel-CE <kalppatel.ce@gmail.com>
2025-05-08 03:21:46 -0700

    Create practical8.1.cpp

---
+++ practical8.1.cpp
@@ -0,0 +1,59 @@
+//This program is prepared by 24ce043_Dev
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void reverseUsingStdReverse(vector<int>& sequence) {
+    reverse(sequence.begin(), sequence.end());
+}
+
+void reverseUsingIterators(vector<int>& sequence) {
+    auto start = sequence.begin();
+    auto end = sequence.end() - 1;
+    while (start < end) {
+        iter_swap(start, end);
+        ++start;
+        --end;
+    }
+}
+
+int main() {
+    vector<int> sequence;
+    int n;
+
+    cout << "Enter the number of elements in the sequence: ";
+    cin >> n;
+
+    cout << "Enter the elements of the sequence:\n";
+    for (int i = 0; i < n; ++i) {
+        int element;
+        cin >> element;
+        sequence.push_back(element);
+    }
+
+    cout << "Original sequence: ";
+    for (const auto& elem : sequence) {
+        cout << elem << " ";
+    }
+    cout << "\n";
+
+    reverseUsingStdReverse(sequence);
+    cout << "Reversed sequence using std::reverse: ";
+    for (const auto& elem : sequence) {
+        cout << elem << " ";
+    }
+    cout << "\n";
+
+    reverseUsingIterators(sequence);
+    cout << "Reversed sequence using iterators: ";
+    for (const auto& elem : sequence) {
+        cout << elem << " ";
+    }
+    cout << "\n";
+
+    cout << "24CE045_Kalp\n";
+    return 0;
+}
